87ad3dfbb1bbb709dca0a13acb25b8f27a1e3c95
Guillaume Sissan <guillaume.sissan.pro@gmail.com>
2022-07-29 15:33:38 +0200

    Ajout extensions

--- Back/index.js
+++ Back/index.js
@@ -2,30 +2,30 @@
  * Pour pouvoir appeler et utiliser les extensions et les routes
  */
 const mongoose = require('mongoose');
-//const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require('express-mongo-sanitize');
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const userRoutes = require("./routes/user");
-const postsRoutes = require("./routes/posts");
+const postsRoutes = require("./roimage.pngutes/posts");
 const path = require('path');
 const dotEnv = require("dotenv");
 dotEnv.config();
-//const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
-/*const limiter = rateLimit({
+const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limiter chaque adresse IP à 100 requêtes toutes les 15mn
     standardHeaders: true, // Renvoyer l'info de la limite atteinte aux `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});*/
+});
 /**
  * permet de monter la fonction middleware spécifiée
  */
 app.use(express.json());
 //app.use(cors());
-//app.use(mongoSanitize());
+app.use(mongoSanitize());
 //app.use(limiter);
 // Routes
 app.use("/api/auth", userRoutes);
